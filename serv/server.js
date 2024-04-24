52c6a62d927176ebe283dcb8d321ed745f64bd04
bogdanmandriichuk <anon858@outlook.com>
2024-04-24 16:18:26 +0300

    baxa

---
+++ serv/server.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+const port = 3001;
+
+const db = new sqlite3.Database('./database.db');
+
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS albums (id INTEGER PRIMARY KEY AUTOINCREMENT, artist TEXT, album TEXT, cover TEXT, country TEXT, youtube_link TEXT, year INTEGER, listened INTEGER DEFAULT 0)");
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const uploadDir = path.join(__dirname, 'uploads');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, uploadDir);
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+const upload = multer({ storage: storage });
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+// Додайте цю строку для обробки статичних файлів з папки uploads
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.route('/api/albums')
+    .get((req, res) => {
+        const { type } = req.query;
+        console.log("Тип запиту:", type);
+
+        if (type === 'listened') {
+            let sql = "SELECT * FROM albums WHERE listened = 1";
+            db.all(sql, (err, rows) => {
+                if (err) {
+                    console.error(err.message);
+                    res.status(500).send('Помилка сервера');
+                } else {
+                    console.log("Результат запиту до бази даних:", rows);
+                    res.json(rows);
+                }
+            });
+        } else if (type === 'to-listen') {
+            let sql = "SELECT * FROM albums WHERE listened = 2"; // Змінено на 2 для ще треба прослухати
+            db.all(sql, (err, rows) => {
+                if (err) {
+                    console.error(err.message);
+                    res.status(500).send('Помилка сервера');
+                } else {
+                    console.log("Результат запиту до бази даних:", rows);
+                    res.json(rows);
+                }
+            });
+        } else {
+            res.status(400).send('Неправильний запит');
+        }
+    })
+    .post(upload.single('cover'), (req, res) => {
+        console.log(req.body);
+        const { action, listened } = req.body;
+        console.log("action:", action);
+        console.log("listened:", listened);
+        // Додавання нового альбому
+        if (action === 'add') {
+            const { artist, album, country, youtube_link, year } = req.body;
+            const listenedValue = listened || 1; // Перевіряємо та конвертуємо значення "listened"
+
+            const cover = req.file ? `http://localhost:3001/uploads/${req.file.filename}` : '';
+
+            console.log("Дані, які надійшли з клієнтського додатку:");
+            console.log("Виконавець:", artist);
+            console.log("Альбом:", album);
+            console.log("Країна:", country);
+            console.log("Посилання на YouTube:", youtube_link);
+            console.log("Рік:", year);
+            console.log("Чи прослуханий:", listenedValue); // Використовуємо конвертоване значення "listened"
+            console.log("Шлях до обкладинки:", cover);
+
+            db.run("INSERT INTO albums (artist, album, cover, country, youtube_link, year, listened) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                [artist, album, cover, country, youtube_link, year, listenedValue],
+                (err) => {
+                    if (err) {
+                        console.error("Помилка при виконанні INSERT-запиту до бази даних:", err.message);
+                        res.status(500).send('Помилка сервера');
+                    } else {
+                        console.log("Альбом успішно доданий до бази даних");
+                        res.status(201).send('Альбом успішно доданий');
+                    }
+                });
+        }
+        // Інші дії, наприклад, оновлення або видалення альбому, можна додати тут
+        else {
+            res.status(400).send('Неправильний запит');
+        }
+    });
+
+app.listen(port, () => {
+    console.log(`Сервер запущений на порту ${port}`);
+});
